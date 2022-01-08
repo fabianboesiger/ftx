6ffc89000174cb0fe95c233a6c72a5b9b13d732e
Michael Vines <mvines@gmail.com>
2022-01-07 20:31:54 -0800

    Add missing GetFills arguments

--- src/rest/model/fills.rs
+++ src/rest/model/fills.rs
@@ -1,32 +1,40 @@
-use super::Request;
+use super::{common::Id, Request};
 use crate::ws::Fill;
+use chrono::{DateTime, Utc};
 use http::Method;
 use serde::Serialize;
-use std::borrow::Cow;
 
 #[derive(Debug, Clone, Serialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct GetFills {
-    #[serde(skip_serializing)]
     pub market_name: String,
+    #[serde(
+        skip_serializing_if = "Option::is_none",
+        serialize_with = "super::serialize_as_timestamp"
+    )]
+    pub start_time: Option<DateTime<Utc>>,
+    #[serde(
+        skip_serializing_if = "Option::is_none",
+        serialize_with = "super::serialize_as_timestamp"
+    )]
+    pub end_time: Option<DateTime<Utc>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub order_id: Option<Id>,
 }
 
 impl GetFills {
     pub fn new(market_name: &str) -> Self {
         Self {
             market_name: market_name.into(),
+            ..Self::default()
         }
     }
 }
 
 impl Request for GetFills {
     const METHOD: Method = Method::GET;
-    const PATH: &'static str = "/fills?market={}";
+    const PATH: &'static str = "/fills";
     const AUTH: bool = true;
 
     type Response = Vec<Fill>;
-
-    fn path(&self) -> Cow<'_, str> {
-        Cow::Owned(format!("/fills?market={}", self.market_name))
-    }
 }
